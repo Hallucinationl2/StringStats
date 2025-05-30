1ed311066ec4f3a1ec716fe594f40262d7c0d7df
Radoslav <117578667+Hallucinationl2@users.noreply.github.com>
2025-05-30 23:43:50 +0300

    Add files via upload

---
+++ stringStats/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class StringStats
+{
+    static void Main()
+    {
+        Console.Write("Въведете текст: ");
+        string input = Console.ReadLine();
+
+        int letters = 0, digits = 0, spaces = 0;
+        foreach (char c in input)
+        {
+            if (char.IsLetter(c)) letters++;
+            else if (char.IsDigit(c)) digits++;
+            else if (char.IsWhiteSpace(c)) spaces++;
+        }
+
+        Console.WriteLine($"\nБукви: {letters}\nЦифри: {digits}\nИнтервали: {spaces}");
+    }
+}
